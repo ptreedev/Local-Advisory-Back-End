1353ed66a5a0bb38cf8936616caa2dc5ba3a1d28
Oisín Wood <oisin@bigbrandideas.co.uk>
2025-02-24 20:22:22 +0000

    Adding UserEvents migration

---
+++ src/database/migrations/20250224183610-create-user-events.ts
@@ -0,0 +1,35 @@
+'use strict';
+
+import { DataTypes, QueryInterface } from "sequelize";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+    await queryInterface.createTable('UserEvents', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      eventId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down (queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+    await queryInterface.dropTable('UserEvents');
+  }
+};
