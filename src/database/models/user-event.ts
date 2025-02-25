10afde2a6b2e8e4f1166310dd59985a500270f00
ptreedev <166836879+ptreedev@users.noreply.github.com>
2025-02-25 11:20:35 +0000

    remove unused imports from user-events

--- src/database/models/user-event.ts
+++ src/database/models/user-event.ts
@@ -1,56 +1,57 @@
-'use strict';
+"use strict";
 
-import { BelongsToMany, DataTypes, Model } from "sequelize";
+import { DataTypes, Model } from "sequelize";
 import sequelizeConnection from "../connection";
-import Event from "./events";
 
 interface UserEventAttributes {
   userId?: number;
   eventId?: number;
-  
 
   id?: number;
   updatedAt?: Date;
   deletedAt?: Date;
   createdAt?: Date;
-};
+}
 
-class UserEvent extends Model<UserEventAttributes> implements UserEventAttributes {
+class UserEvent
+  extends Model<UserEventAttributes>
+  implements UserEventAttributes
+{
   public id!: number;
 
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
 }
 
-
-UserEvent.init({
-  id: {
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-    type: DataTypes.NUMBER
+UserEvent.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.NUMBER,
+    },
+    eventId: {
+      allowNull: false,
+      type: DataTypes.NUMBER,
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.NUMBER,
+    },
+    createdAt: {
+      allowNull: true,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: true,
+      type: DataTypes.DATE,
+    },
   },
-  eventId: {
-    allowNull: false,
-    type: DataTypes.NUMBER
-  },
-  userId: {
-    allowNull: false,
-    type: DataTypes.NUMBER
-  },
-  createdAt: {
-    allowNull: true,
-    type: DataTypes.DATE
-  },
-  updatedAt: {
-    allowNull: true,
-    type: DataTypes.DATE
+  {
+    sequelize: sequelizeConnection,
+    modelName: "UserEvent",
   }
+);
 
-}, {
-  sequelize: sequelizeConnection,
-  modelName: 'UserEvent',
-});
-
-
-export default UserEvent
+export default UserEvent;
