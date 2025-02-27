bf44905f154b486a012e2eea5d85d8b80659554f
ptreedev <166836879+ptreedev@users.noreply.github.com>
2025-02-27 11:28:24 +0000

    update associations for category and event

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,3 +1,5 @@
+import Category from "./category";
+import EventCategory from "./event-categories";
 import Event from "./events";
 import Location from "./location";
 import User from "./user";
@@ -6,22 +8,34 @@ import UserEvent from "./user-event";
 Location.hasMany(Event);
 Event.belongsTo(Location);
 User.hasMany(Event, {
-    foreignKey: 'ownerId',
-    as: 'events'
-}); 
+  foreignKey: "ownerId",
+  as: "events",
+});
 Event.belongsTo(User, {
-    foreignKey: 'ownerId',
-    as: 'Owner'
+  foreignKey: "ownerId",
+  as: "Owner",
 });
 User.belongsToMany(Event, {
-    as: 'registeredEvents',
-    through: UserEvent,
-    foreignKey: 'eventId',
-    otherKey: 'userId'
-}); 
+  as: "registeredEvents",
+  through: UserEvent,
+  foreignKey: "eventId",
+  otherKey: "userId",
+});
 Event.belongsToMany(User, {
-    as: 'attendees',
-    through: UserEvent,
-    foreignKey: 'userId',
-    otherKey: 'eventId'
-})
+  as: "attendees",
+  through: UserEvent,
+  foreignKey: "userId",
+  otherKey: "eventId",
+});
+Category.belongsToMany(Event, {
+  as: "Events",
+  through: EventCategory,
+  foreignKey: "eventId",
+  otherKey: "categoryId",
+});
+Event.belongsToMany(Category, {
+  as: "category",
+  through: EventCategory,
+  foreignKey: "categoryId",
+  otherKey: "eventId",
+});
