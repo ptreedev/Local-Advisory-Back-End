024cef16b46bd4dc4ff470cebe8edfea9cc6f5d0
ptreedev <166836879+ptreedev@users.noreply.github.com>
2025-02-27 11:31:00 +0000

    create music category and call addCategory method

--- src/index.ts
+++ src/index.ts
@@ -2,43 +2,49 @@ import Express from "express";
 import User from "./database/models/user";
 import { PORT } from "./configs";
 import Event from "./database/models/events";
-import Location from "./database/models/location"
-import './database/models';
+import Location from "./database/models/location";
+import "./database/models";
+import Category from "./database/models/category";
 
 const server = Express();
 const port = PORT || 8080;
 server.get("/", async (req, res) => {
-    const user = await User.create({
-        firstName: "John",
-        lastName: "Smith",
-        email: "john@smith.co.uk",
-        password: "johnsmith1"
-    });
-    const location = await Location.create({
-        name: "First Direct",
-        addressLine1: "12 Here Street",
-        addressLine2: "Manchester",
-        city: "Manchester",
-        postCode: "M1 2TB",
-        county: "Greater Manchester"
-    });
-    const event = await Event.create({
-        name: 'NewEvent',
-        description: '',
-        dateFrom: new Date(),
-        ownerId: user.id,
-        timeStart: new Date(),
-        locationId: location.id,
-    });
-    event.addAttendee(user);
-    res.send(await Event.findOne({
-        where: {
-            id: event.id
-        },
-        include: [Location, 'attendees']
-    }))
-})
+  const user = await User.create({
+    firstName: "John",
+    lastName: "Smith",
+    email: "john@smith.co.uk",
+    password: "johnsmith1",
+  });
+  const location = await Location.create({
+    name: "First Direct",
+    addressLine1: "12 Here Street",
+    addressLine2: "Manchester",
+    city: "Manchester",
+    postCode: "M1 2TB",
+    county: "Greater Manchester",
+  });
+  const category = await Category.create({
+    name: "Music",
+  });
+  const event = await Event.create({
+    name: "NewEvent",
+    description: "",
+    dateFrom: new Date(),
+    ownerId: user.id,
+    timeStart: new Date(),
+    locationId: location.id,
+  });
+  event.addAttendee(user);
+  event.addCategory(category);
+  res.send(
+    await Event.findOne({
+      where: {
+        id: event.id,
+      },
+      include: [Location, "attendees", "category"],
+    })
+  );
+});
 server.listen(port, () => {
-    console.log(`Server is listeing on ${port}`)
-})
-
+  console.log(`Server is listeing on ${port}`);
+});
