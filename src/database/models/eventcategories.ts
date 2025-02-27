0883768beb242114a350165abb2566581f627a90
ptreedev <166836879+ptreedev@users.noreply.github.com>
2025-02-27 09:44:13 +0000

    create EventCategories migration and EventCategory model

---
+++ src/database/models/eventcategories.ts
@@ -0,0 +1,55 @@
+"use strict";
+
+import { DataTypes, Model } from "sequelize";
+import sequelizeConnection from "../connection";
+
+interface EventCategoryAttributes {
+  categoryId?: number;
+  eventId?: number;
+
+  id?: number;
+  updatedAt?: Date;
+  deletedAt?: Date;
+  createdAt?: Date;
+}
+class EventCategory
+  extends Model<EventCategoryAttributes>
+  implements EventCategoryAttributes
+{
+  public id!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+EventCategory.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.NUMBER,
+    },
+    eventId: {
+      allowNull: false,
+      type: DataTypes.NUMBER,
+    },
+    categoryId: {
+      allowNull: false,
+      type: DataTypes.NUMBER,
+    },
+    createdAt: {
+      allowNull: true,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: true,
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize: sequelizeConnection,
+    modelName: "EventCategory",
+  }
+);
+
+export default EventCategory;
