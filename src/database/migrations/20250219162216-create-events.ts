993521b0b520813a4779f63e60ded62b6603fa6c
ptreedev <166836879+ptreedev@users.noreply.github.com>
2025-02-27 11:27:28 +0000

    create event-categories model and edit event migration to include category

--- src/database/migrations/20250219162216-create-events.ts
+++ src/database/migrations/20250219162216-create-events.ts
@@ -1,73 +1,82 @@
-'use strict';
+"use strict";
 
 import { DataTypes, QueryInterface } from "sequelize";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
-    await queryInterface.createTable('Events', {
+    await queryInterface.createTable("Events", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       locationId: {
-        type: Sequelize.INTEGER, 
-        references: { 
+        type: Sequelize.INTEGER,
+        references: {
           model: {
-            tableName: "Locations"
+            tableName: "Locations",
           },
-          key: "id"
-        }
+          key: "id",
+        },
       },
       ownerId: {
-        type: Sequelize.INTEGER, 
-        references: { 
+        type: Sequelize.INTEGER,
+        references: {
           model: {
-            tableName: "Users"
+            tableName: "Users",
           },
-          key: "id"
-        }
+          key: "id",
+        },
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Categories",
+          },
+          key: "id",
+        },
       },
       image: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       dateFrom: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       dateTo: {
         allowNull: true,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       price: {
         allowNull: true,
-        type: Sequelize.NUMBER
+        type: Sequelize.NUMBER,
       },
       timeStart: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       timeEnd: {
         type: Sequelize.DATE,
-        allowNull: true
+        allowNull: true,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
-    await queryInterface.dropTable('Events');
-  }
+    await queryInterface.dropTable("Events");
+  },
 };
